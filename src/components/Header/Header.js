c021444d424368d73a5db5a9d40ad68ab21f0a08
Tzhuraveel <timofiyzhuravel2003@gmail.com>
2023-01-30 22:19:13 +0200

    homework4

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,15 @@
+import {NavLink} from "react-router-dom";
+
 import css from './Header.module.css'
-import {Link} from "react-router-dom";
+
 
 const Header = () => {
     return (
         <div className={css.container__header}>
             <div className={css.header}>
-                <Link to={'todos'}>Todos</Link>
-                <Link to={'albums'}>Albums</Link>
-                <Link to={'comments'}>Comments</Link>
+                <NavLink to={'todos'}>Todos</NavLink>
+                <NavLink to={'albums'}>Albums</NavLink>
+                <NavLink to={'comments'}>Comments</NavLink>
             </div>
         </div>
     );
