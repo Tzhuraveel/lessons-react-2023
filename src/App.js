6f6aaa4fe7762e4584490a0bdf83107ab79a429d
Tzhuraveel <timofiyzhuravel2003@gmail.com>
2023-01-31 18:16:57 +0200

    bonus5

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import {Route, Routes} from "react-router-dom";
+import {MainLayout} from "./layout";
+import {CommentsPage, HomePage, LoginPage, PostsPage, UsersPage} from "./pages";
+import {Required} from "./hoc";
 
 function App() {
   return (
-    <div className="App">
+       <Routes>
+         <Route path={'/'} element={<MainLayout/>}>
+           <Route index element={<HomePage/>}/>
+           <Route path={'users'} element={<UsersPage/>}/>
+           <Route path={'posts'} element={<Required>
+               <PostsPage/>
+           </Required>}>
 
-    </div>
+           </Route>
+           <Route path={'comments'} element={<CommentsPage/>}/>
+           <Route path={'/login'} element={<LoginPage/>}/>
+         </Route>
+       </Routes>
   );
 }
 
